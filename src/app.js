aacd5544619f7f471a4b04e0264c294efe38d68a
22120255 <22120255@student.hcmus.edu.vn>
2024-11-01 14:04:33 +0700

    setup database

--- src/app.js
+++ src/app.js
@@ -4,13 +4,23 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const route = require('./routes');
+const cors = require('cors');  
 const { engine } = require('express-handlebars');
+const db = require('./config/db');  
 
+// Connect to DB
+db.connectDB();
 
+// Body parser
+app.use(cors());
+app.use(express.json());
+
+// Static file
 app.use(express.static(path.join(__dirname, "public")));
 
 // HTTP logger
 app.use(morgan("dev"));
+
 // Template engine
 app.engine(
     "hbs",
@@ -25,8 +35,11 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
 
+// Route init
 route(app);
 
+
+// Listen to port
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 })
