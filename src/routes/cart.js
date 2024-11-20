728ba06db732c2e5e53963e6a15336aa98ec218f
nhankhtn <nguyenanhnguyen3006@gmail.com>
2024-11-20 20:48:37 +0700

    fix route

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -4,6 +4,6 @@ const router = express.Router()
 const cartController = require('../controllers/CartController')
 const { isAuthenticated } = require('../middleware/authMiddleware')
 router.get('/', cartController.getCart)
-router.post('/add/:product-id', isAuthenticated, cartController.addToCart)
+router.post('/add/:productId', isAuthenticated, cartController.addToCart)
 
 module.exports = router
