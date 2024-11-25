08bb91a8aec57d83840a83074f6ae19cbafc4798
nhankhtn <nguyenanhnguyen3006@gmail.com>
2024-11-26 00:16:36 +0700

    update: cache middleware

--- src/routes/site.js
+++ src/routes/site.js
@@ -4,9 +4,10 @@ const router = express.Router()
 const siteController = require('../controllers/SiteController')
 const { isAuthenticated } = require('../middleware/authMiddleware')
 const { uploadImage } = require('../config/multer')
+const cacheMiddleware = require('../middleware/cacheMiddleware')
 
-router.get('/profile/:id', isAuthenticated, siteController.profile)
+router.get('/profile/:id', isAuthenticated, cacheMiddleware, siteController.profile)
 router.patch('/user/avatar/store', uploadImage.single('avatar'), siteController.updateAvatar)
-router.get('/', siteController.index)
+router.get('/', cacheMiddleware, siteController.index)
 
 module.exports = router
