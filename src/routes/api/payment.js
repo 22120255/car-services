0417dfe41182370c77afbcc8ac925d3c45bc8a47
nhankhtn <nguyenanhnguyen3006@gmail.com>
2024-12-04 21:04:57 +0700

    refactor cart

--- src/routes/api/payment.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const paymentController = require('../../controllers/PaymentController')
-const { isAuthenticated } = require('../../middleware/authMiddleware')
-
-router.get('/createQR', isAuthenticated, paymentController.createQR);
-
-module.exports = router
--- src/routes/ui/payment.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const paymentController = require('../../controllers/PaymentController')
-const { isAuthenticated } = require('../../middleware/authMiddleware')
-
-router.get('/:cartID', isAuthenticated, paymentController.payment)
-
-module.exports = router;
