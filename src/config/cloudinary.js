382d4655c3e8874b5d0e3d45505367728d30891d
22120255 <22120255@student.hcmus.edu.vn>
2024-12-08 14:48:17 +0700

    nothing change

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,11 +1,16 @@
-const cloudinary = require("cloudinary").v2;
-
+const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-
+cloudinary.api.resources((error, result) => {
+  if (error) {
+    console.error('Cloudinary Connection Failed:', error);
+  } else {
+    console.log('Cloudinary Connection Successful:', result);
+  }
+});
 module.exports = cloudinary;
--- src/config/multer.js
+++ src/config/multer.js
@@ -1,15 +1,26 @@
-const cloudinary = require('./cloudinary')
-const { CloudinaryStorage } = require('multer-storage-cloudinary')
-const multer = require('multer')
+const cloudinary = require('./cloudinary');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
 
-const storage = new CloudinaryStorage({
-    cloudinary,
-    params: {
-        folder: 'avatars',
-        allowed_formats: ['jpg', 'jpeg', 'png', 'gif', 'webp', 'svg', 'bmp'],
-        transformation: [{ width: 400, height: 400, crop: 'fill' }],
-    },
-})
-const uploadImage = multer({ storage })
+const avatarStorage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'avatars', // Folder dành cho ảnh đại diện
+    allowed_formats: ['jpg', 'jpeg', 'png', 'gif', 'webp', 'svg', 'bmp'],
+    transformation: [{ width: 400, height: 400, crop: 'fill' }],
+  },
+});
 
-module.exports = { uploadImage }
+const productStorage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'products', // Folder dành cho sản phẩm
+    allowed_formats: ['jpg', 'jpeg', 'png', 'gif', 'webp', 'svg', 'bmp'],
+    transformation: [{ width: 800, height: 800, crop: 'fill' }],
+  },
+});
+
+const uploadImage = multer({ avatarStorage });
+const uploadProductImage = multer({ productStorage });
+
+module.exports = { uploadImage, uploadProductImage };
