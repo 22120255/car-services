94556cdbf22ac95624ba52714af109212c1327ea
buonnguwaaa <nhannguyen04.it@gmail.com>
2024-12-17 20:41:11 +0700

    fix

--- src/public/js/shops-cart/index.js
+++ src/public/js/shops-cart/index.js
@@ -60,18 +60,18 @@ document.addEventListener('DOMContentLoaded', async function () {
             }),
             success: function (response) {
               console.log('Order Response:', response);
-              // if (response.order) {
-              //   // Chuyển hướng đến URL thanh toán
-              //   const paymentUrl = response.order
-              //     ? `/payment/create_payment_url?amount=${response.order.totalAmount}&orderId=${response.order._id}`
-              //     : response.paymentUrl;
-              //   window.location.href = paymentUrl;
-              // } else {
+              if (response.order) {
+                // Chuyển hướng đến URL thanh toán
+                const paymentUrl = response.order
+                  ? `/payment/create_payment_url?amount=${response.order.totalAmount}&orderId=${response.order._id}`
+                  : response.paymentUrl;
+                window.location.href = paymentUrl;
+              } else {
                 showModal({
                   title: 'Lỗi',
                   content: 'Không thể tạo đơn hàng. Vui lòng thử lại.',
                 });
-              // }
+              }
             },
             error: function (xhr, status, error) {
               console.error('Order Error:', error);
