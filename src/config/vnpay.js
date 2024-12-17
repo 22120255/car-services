70b80c9256c2b53672614b26569b638ff2602dbb
buonnguwaaa <nhannguyen04.it@gmail.com>
2024-12-18 00:31:43 +0700

    fix payment url

--- src/config/vnpay.js
+++ src/config/vnpay.js
@@ -4,7 +4,7 @@ let config = {
     vnp_HashSecret: process.env.VNP_HASHSECRET,
     vnp_Url: process.env.VNP_URL,
     vnp_ReturnUrl: process.env.VNP_RETURN_URL,
-    vnp_IpnUrl: "https://car-services-61gu.onrender.com/api/payment/vnpay_ipn"
+    vnp_IpnUrl: process.env.VNP_IPN_URL 
 };
 
 function updateIpnUrl(newUrl) {
