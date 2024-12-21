c2b0a3108ed2b26b5b78063bbcaf5d4a3817d1c5
nhankhtn <nguyenanhnguyen3006@gmail.com>
2024-12-21 12:04:39 +0700

    hot fix custom log

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,16 +1,18 @@
 const { createLogger, format, transports } = require('winston');
 
-const logger = createLogger({
-    level: 'error',
-    format: format.combine(
-        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-        format.printf(({ timestamp, level, message }) => {
-            return `${timestamp} [${level.toUpperCase()}]: ${message}`;
-        })
-    ),
-    transports: [
-        new transports.File({ filename: 'src/logs/error.log' }),
-    ]
-});
+const logger = (type) => {
+    return createLogger({
+        level: type,
+        format: format.combine(
+            format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+            format.printf(({ timestamp, level, message }) => {
+                return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+            })
+        ),
+        transports: [
+            new transports.File({ filename: `src/logs/${type}.log` }),
+        ]
+    })
+}
 
 module.exports = logger;
