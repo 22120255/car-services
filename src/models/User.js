2b87b1efa3a11459fd03d68a01cd2312cf68dead
buonnguwaaa <nhannguyen04.it@gmail.com>
2024-12-29 19:27:47 +0700

    insert OrderId in metadata

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const { ObjectID } = require('mongodb');
 
 const UserSchema = new mongoose.Schema(
   {
@@ -35,6 +36,10 @@ const UserSchema = new mongoose.Schema(
       address: { type: String },
       purchasedProducts: [
         {
+          orderId: { 
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Order',
+          },
           product: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Product',
